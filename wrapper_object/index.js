111912317a3ff658dc463421fdf21a8bc974d5a2
nabe-kurage <tabiflag@gmail.com>
2020-12-22 02:46:10 +0900

    add

--- wrapper_object/index.js
+++ wrapper_object/index.js
@@ -6,4 +6,9 @@ console.log(test);
 
 const str = new String('input vue');
 console.log(str.toUpperCase());
-/////
+//========================================================
+
+const str2 = "文字列";
+console.log(typeof str2);
+const stringWrapper = new String("文字列");
+console.log(typeof stringWrapper);
