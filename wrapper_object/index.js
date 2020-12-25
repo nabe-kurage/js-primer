3b6686e3d1f283a33be2fa4f011138ecd1efcd85
nabe-kurage <tabiflag@gmail.com>
2020-12-25 23:59:33 +0900

    ラッパーオブジェクトについて

--- wrapper_object/index.js
+++ wrapper_object/index.js
@@ -6,9 +6,14 @@ console.log(test);
 
 const str = new String('input vue');
 console.log(str.toUpperCase());
+
 //========================================================
 
+// プリミテゥブ型をオブジェクト型に変更するラッパーオブジェクトという物が存在する。
 const str2 = "文字列";
 console.log(typeof str2);
 const stringWrapper = new String("文字列");
 console.log(typeof stringWrapper);
+
+//========================================================
+
