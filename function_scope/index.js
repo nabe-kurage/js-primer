59163a0995a6a74f0cfaf9afa2addf92adc6e916
nabe-kurage <tabiflag@gmail.com>
2021-01-01 23:42:25 +0900

    check leen code

--- function_scope/index.js
+++ function_scope/index.js
@@ -1,10 +1,34 @@
-// スコープは参照できる範囲を決める物
-function fn() {
-    const x = 1;
-    console.log(x);
-}
-fn();
-//ここで呼ぶとエラー
-// console.log(x);
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ // [7,2,5,3,6,4,1]
+//  差分を持っておく必要がある？
+// →そうか全ての値で最大値を持っておけばいいんだ
+var isValid = function(s) {
+    const letterSets =  {'(':')', '{':'}', '[':']'};
+    const guess = [];
+    for (i = 0; i < s.length; i++){
+        console.log(letterSets.hasOwnProperty(s[i]));
 
-//========================================================
+        if (s.hasOwnProperty(s[i])) {
+            
+            guess.push(letterSets[s[i]]);
+        } else {
+            if(guess[guess.length - 1] === s[i]){
+                guess.pop();
+            }else{
+                return false;
+            }
+        }
+    }
+    
+    if(guess.length > 0){
+       return false;
+    }
+    return true;
+};
+
+
+console.log(isValid("()"));
